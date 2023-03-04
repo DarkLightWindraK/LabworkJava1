2447d6cd5888402182431c6b347b43c0aa530538
DarkLightWindraK <DarkLightWindraK@yandex.ru>
2023-03-04 23:53:39 +0300

    Add java docs

--- src/CustomLinkedList.java
+++ src/CustomLinkedList.java
@@ -1,12 +1,29 @@
 import java.util.NoSuchElementException;
 
+/**
+ * A custom implementation of a linked list.
+ *
+ * @param <E> the type of elements stored in the linked list
+ */
 public class CustomLinkedList<E> {
 
+    /**
+     * A node containing an element of type E.
+     *
+     * @param <E> the type of element stored in the node
+     */
     public static class Node<E> {
         E item;
         Node<E> next;
         Node<E> prev;
 
+        /**
+         * Constructor for creating a new node with a given previous node, element, and next node.
+         *
+         * @param prev    the previous node of the new node
+         * @param element the element stored in the new node
+         * @param next    the next node of the new node
+         */
         Node(Node<E> prev, E element, Node<E> next) {
             this.item = element;
             this.next = next;
@@ -14,13 +31,22 @@ public class CustomLinkedList<E> {
         }
     }
 
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+
+    /**
+     * Constructor for creating a new linked list with size 0.
+     */
     public CustomLinkedList() {
         size = 0;
     }
 
-    Node<E> first, last;
-    int size;
-
+    /**
+     * Adds an element to the end of the linked list.
+     *
+     * @param element the element to add to the linked list
+     */
     public void add(E element) {
         final Node<E> tempLast = last;
         final Node<E> newNode = new Node<>(tempLast, element, null);
@@ -32,7 +58,14 @@ public class CustomLinkedList<E> {
         size++;
     }
 
-    public E get(int index) {
+    /**
+     * Gets the element at the specified index of the linked list.
+     *
+     * @param index the index of the element to retrieve
+     * @return the element at the specified index
+     * @throws IndexOutOfBoundsException if the index is larger than or equal to the size of the linked list
+     */
+    public E get(int index) throws IndexOutOfBoundsException {
         if (index >= size) {
             throw new IndexOutOfBoundsException();
         }
@@ -44,7 +77,14 @@ public class CustomLinkedList<E> {
         return current.item;
     }
 
-    public E remove(int index) {
+    /**
+     * Removes the element at the specified index from the linked list.
+     *
+     * @param index the index of the element to remove
+     * @return the removed element
+     * @throws IndexOutOfBoundsException if the index is larger than or equal to the size of the linked list
+     */
+    public E remove(int index) throws IndexOutOfBoundsException {
         if (index >= size) {
             throw new IndexOutOfBoundsException();
         }
@@ -61,7 +101,14 @@ public class CustomLinkedList<E> {
         return savedValue;
     }
 
-    public E remove(E element) {
+    /**
+     * Removes the specified element from the linked list.
+     *
+     * @param element the element to remove from the linked list
+     * @return the removed element
+     * @throws NoSuchElementException if the specified element is not found in the linked list
+     */
+    public E remove(E element) throws NoSuchElementException {
         Node<E> current = first;
         while (current != null && current.item != element) {
             current = current.next;
